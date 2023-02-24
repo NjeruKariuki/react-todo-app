bd577b974bf7b71e439440473b640964252582bc
Chief <freakoutbond2@gmail.com>
2023-02-24 20:50:00 +0300

    task component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Task from "./assets/Task";
+import { useState } from "react";
 
 function App() {
+  //tasks array and is complete status states
+  const {tasks, setTasks} = useState([]);
+  const {isComplete, toggleComplete} = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container w-screen h-screen bg-white flex justify-center items-center">
+      <div className="header w-1/2 h-1/2 bg-slate-400 rounded-md">
+        <h1 className="text-2xl text-blue-700">To do list app</h1>
+        <div className="inputsForm">
+          <form>
+            <input type="text" name="" value=""/>
+            <button onClick={handleSubmit}>Submit</button>
+          </form>
+        </div>
+
+        <div className="tasks">
+          <ul>
+              <Task completed={completed} />
+          </ul>
+        </div>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/assets/Task.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Task(props) {
+
+    const handleDelete = (props) => {
+        
+    }
+  return (
+    <div>
+        <li>Task 1</li>
+        <button type="submit" onClick={handleDelete}>X</button>
+    </div>
+  )
+}
+
+export default Task
